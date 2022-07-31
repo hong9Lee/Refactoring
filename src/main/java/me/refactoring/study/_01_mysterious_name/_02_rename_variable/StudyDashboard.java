7410503e9554f1770f4adf377c5c4a03b883f998
hong9Lee <hongtemp9mac@gmail.com>
2022-07-31 22:58:47 +0900

    first commit

---
+++ src/main/java/me/refactoring/study/_01_mysterious_name/_02_rename_variable/StudyDashboard.java
@@ -0,0 +1,60 @@
+package me.refactoring.study._01_mysterious_name._02_rename_variable;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class StudyDashboard {
+
+    private Set<String> usernames = new HashSet<>();
+
+    private Set<String> reviews = new HashSet<>();
+
+    /**
+     * 스터디 리뷰 이슈에 작성되어 있는 리뷰어 목록과 리뷰를 읽어옵니다.
+     */
+    private void loadReviews() throws IOException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("me/live-study");
+        GHIssue issue = repository.getIssue(30);
+
+//        List<GHIssueComment> comments = issue.getComments();
+//        for (GHIssueComment comment : comments) {
+//            usernames.add(comment.getUserName());
+//            this.reviews.add(comment.getBody());
+//        }
+
+        /**
+         * 리뷰를 읽어오는 함수이기 떄문에 좀 더 구체적인 네이밍
+         */
+        List<GHIssueComment> reviews = issue.getComments();
+        for (GHIssueComment review : reviews) {
+            usernames.add(review.getUserName());
+            this.reviews.add(review.getBody());
+        }
+
+    }
+
+    public Set<String> getUsernames() {
+        return usernames;
+    }
+
+    public Set<String> getReviews() {
+        return reviews;
+    }
+
+    public static void main(String[] args) throws IOException {
+        StudyDashboard studyDashboard = new StudyDashboard();
+        studyDashboard.loadReviews();
+
+
+        studyDashboard.getUsernames().forEach(name -> System.out.println(name));
+        studyDashboard.getReviews().forEach(review -> System.out.println(review));
+    }
+}
