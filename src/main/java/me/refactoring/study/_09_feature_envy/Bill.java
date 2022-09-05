a84df14a9df3d9f9b291e2146eb082f059eabd62
hong9Lee <hongtemp9mac@gmail.com>
2022-09-05 18:07:55 +0900

    "9. 기능 편애 commit

---
+++ src/main/java/me/refactoring/study/_09_feature_envy/Bill.java
@@ -0,0 +1,17 @@
+package me.refactoring.study._09_feature_envy;
+
+public class Bill {
+
+    private ElectricityUsage electricityUsage;
+
+    private GasUsage gasUsage;
+
+    public double calculateBill() {
+        return electricityUsage.getElecticityBill() + gasUsage.getGasBill();
+    }
+
+
+
+
+
+}
---
+++ src/main/java/me/refactoring/study/_09_feature_envy/package-info.java
@@ -0,0 +1,1 @@
+package me.refactoring.study._09_feature_envy;
