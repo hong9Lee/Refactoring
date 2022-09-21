4c4c6c20f2b49e1324c1d15c2d68b470dbde6f67
hong9Lee <hongtemp9mac@gmail.com>
2022-09-21 15:08:50 +0900

    "12. 반복문 commit

---
+++ src/main/java/me/refactoring/study/_13_loop/_33_replace_loop_with_pipeline/Author.java
@@ -0,0 +1,27 @@
+package me.refactoring.study._13_loop._33_replace_loop_with_pipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Author {
+
+    private String company;
+
+    private String twitterHandle;
+
+    public Author(String company, String twitterHandle) {
+        this.company = company;
+        this.twitterHandle = twitterHandle;
+    }
+
+    static public List<String> TwitterHandles(List<Author> authors, String company) {
+        return authors.stream()
+                .filter(author -> author.company.equals(company))
+                .map(author -> author.twitterHandle)
+                .filter(t -> t != null)
+                .collect(Collectors.toList());
+    }
+
+}
---
+++ src/main/java/me/refactoring/study/_13_loop/package-info.java
@@ -0,0 +1,1 @@
+package me.refactoring.study._13_loop;
